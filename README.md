c3c731088c234efa47262aef5f06d8d9bf0f4084
Rupert Smith <rupert@thesett.com>
2018-04-24 14:51:11 +0100

    Documented.

---
+++ README.md
@@ -0,0 +1,4 @@
+# elm-multi-dict
+
+A data structure for representing one-to-many relationships as a `Dict` of `Set`s of
+values.
--- src/MultiDict.elm
+++ src/MultiDict.elm
@@ -1,13 +1,28 @@
 module MultiDict exposing (MultiDict, insert, remove, get)
 
+{-| MultiDict implements a one-to-many relationship, between `comparable` keys
+and `Set`s of values. Each key may be associated with zero or many values, compared
+with a `Dict` which associated each key with just one value. Its implementation is
+a `Dict` of `Set`s, but the overhead of creating new sets or removing empty sets as
+values are added to or removed from the data structyre is managed for you.
+
+@docs MultiDict, insert, remove, get
+
+-}
+
 import Dict exposing (Dict)
 import EverySet exposing (EverySet)
 
 
+{-| The type of multi dicts from comparable keys to sets of values.
+-}
 type alias MultiDict comparable value =
     Dict comparable (EverySet value)
 
 
+{-| Adds a key-value pair into the dictionary. It is added to the set of values
+already associated with that key.
+-}
 insert :
     comparable
     -> value
@@ -23,6 +38,9 @@ insert k v dict =
         Dict.insert k set dict
 
 
+{-| Removes a key-value pair from the dictionary. It is removed from the set of values
+associated with the key.
+-}
 remove :
     comparable
     -> value
@@ -41,6 +59,9 @@ remove k v dict =
             Dict.insert k set dict
 
 
+{-| Gets the set of values associated with a key, or `Nothing` if there is no
+set of values.
+-}
 get :
     comparable
     -> MultiDict comparable value
